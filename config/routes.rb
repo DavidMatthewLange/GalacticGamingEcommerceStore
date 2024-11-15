c5a047e58130fffe65ee5a73e541049045b8603b
David Matthew Lange <david.m.lange@outlook.com>
2024-11-15 06:21:38 -0600

    Completed Products controller, routes, and views (index, show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :products, only: [ :index, :show ]
+  get "products/index"
+  get "products/show"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +13,6 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+   # Defines the root path route ("/")
+   root "products#index"
 end
