0ba3b067c3bc1eebd9a7581305b16be284fe17b0
David Matthew Lange <david.m.lange@outlook.com>
2024-12-13 16:05:44 -0600

    Pagination implementation is successful

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,21 +3,25 @@ class ProductsController < ApplicationController
     @categories = Category.all
     @products = Product.all
 
-    # Search for products matching the keyword
-    return if params[:search].blank?
+    # Apply search if the search parameter is provided
+    if params[:search].present?
+      @products = @products.where(
+        "products.name LIKE ? OR products.description LIKE ?",
+        "%#{params[:search]}%", "%#{params[:search]}%"
+      )
+    end
 
-    @products = @products.where(
-      "products.name LIKE ? OR products.description LIKE ?",
-      "%#{params[:search]}%", "%#{params[:search]}%"
-    )
+    # Apply category filter if the category parameter is provided
+    if params[:category].present? && params[:category] != "all"
+      @products = @products.joins(:categories).where(categories: { id: params[:category] })
+    end
 
-    # Search products matching the category only
-    return unless params[:category].present? && params[:category] != "all"
-
-    @products = @products.joins(:categories).where(categories: { id: params[:category] })
-
-    # Pagination
+    # Apply pagination
     @products = @products.page(params[:page]).per(20)
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def show
