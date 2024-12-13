2bc8d18d243432ff3d9b7b1e470a8367457a7762
David Matthew Lange <david.m.lange@outlook.com>
2024-12-13 15:44:13 -0600

    Added search function to products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    @categories = Category.all
     @products = Product.all
+
+    return unless params[:search].present?
+
+    @products = @products.where(
+      "products.name LIKE ? OR products.description LIKE ?",
+      "%#{params[:search]}%", "%#{params[:search]}%"
+    )
+
+    return unless params[:category].present? && params[:category] != "all"
+
+    @products = @products.joins(:categories).where(categories: { id: params[:category] })
   end
 
   def show
